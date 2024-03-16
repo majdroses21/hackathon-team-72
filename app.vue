c24a96f7805e8d30aa17f42d7743fd813db1727f
Mohamed Hazem <125741147+mohamed-hazem-fathy@users.noreply.github.com>
2024-03-17 00:53:22 +0200

    last

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-h1">Hello From app.vue</h1>
+    <buttons-group />
+  </div>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["vuetify/styles/main.sass", "@mdi/font/css/materialdesignicons.css"],
+  build: {
+    transpile: ["vuetify"],
+  },
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+
+})
+// export default {
+//   devtools: { enabled: true },
+//   css: ["vuetify/styles/main.sass", "@mdi/font/css/materialdesignicons.css"],
+//   build: {
+//     transpile: ["vuetify"],
+//   }
+// }
